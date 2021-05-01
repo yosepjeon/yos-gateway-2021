afb6478ff285c45f4de1efedf7c177320df49bc3
yosepjeon <enekelx1@naver.com>
2021-05-01 20:53:47 +0900

    Initial commit

---
+++ src/main/java/com/yosep/gateway/YosGateway2021Application.java
@@ -0,0 +1,13 @@
+package com.yosep.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosGateway2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosGateway2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosep/gateway/YosGateway2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosep.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosGateway2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
