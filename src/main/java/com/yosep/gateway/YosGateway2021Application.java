b91e052c3716010b57810d62ed4afb1de095538c
yosepjeon <enekelx1@naver.com>
2021-05-03 19:38:03 +0900

    config server 연동 완료 및 spring cloud bus 연동 완료

--- src/main/java/com/yosep/gateway/YosGateway2021Application.java
+++ src/main/java/com/yosep/gateway/YosGateway2021Application.java
@@ -1,13 +1,20 @@
 package com.yosep.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class YosGateway2021Application {
+public class  YosGateway2021Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YosGateway2021Application.class, args);
 	}
 
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
